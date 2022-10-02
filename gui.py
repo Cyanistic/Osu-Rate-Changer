39237dc080d512196f409bb343b2c1ab1c61e818
Cyanism <61608977+Cyanistic@users.noreply.github.com>
2022-10-02 18:14:31 -0400

    added gui
    fully functioning gui version added

--- gui.py
+++ gui.py
@@ -1,11 +1,111 @@
 import PySimpleGUI as sg
+from pydub import AudioSegment
 import os
 
-def generateFile(audioPath,Rate):
-    pass
+# Function to make generate a new audio file with a speed change from old file
+def speedChange(audio, rate):
+    return audio._spawn(
+        audio.raw_data, overrides={"frame_rate": int(audio.frame_rate * rate)}
+    )
+
+# Function to find the nth occurance of a character in a string
+def trimmer(string, divider, times):
+    index = 0
+    for i in range(times):
+        index = string.find(divider, index)
+        index += 1
+    return index
+
+def generateFiles(path, mapFile, rate):
+    mapData = open(mapFile, "r").readlines()
+
+    for line in range(len(mapData)):
+        if mapData[line].startswith("AudioFilename:"):
+            audio = mapData[line][14 : mapData[line].find(".mp3")].strip()
+            mapData[line] = f"{mapData[line][:14]}{audio} ({rate}x).mp3\n"
+        elif mapData[line].startswith("PreviewTime:"):
+            prevTime = int(mapData[line][12:].strip())
+            mapData[line] = f"{mapData[line][:12]} {int(prevTime/rate)}\n"
+        elif mapData[line].startswith("Mode:"):
+            gamemode = int(mapData[line][5:].strip())
+        elif mapData[line].startswith("Version:"):
+            diffName = mapData[line][8:].strip()
+            mapData[line] = f"{mapData[line][:8]}{diffName} ({rate}x)\n"
+            break
+
+    # Uses the formula for the corresponding gamemode and modifies the rate of the map in the .osu file
+    startIndex = mapData.index("[HitObjects]\n") + 1
+    if gamemode == 0:
+        arPopup = sg.Window("AR Popup", [[sg.Text("Enter new AR (Enter 0 to leave unchanged)")],[sg.Input(key="arBox")],[sg.Button("Submit")],[sg.Text("", key = "result")]])
+        while True:
+            event, values = arPopup.read()
+
+            if event == sg.WIN_CLOSED:
+                break
+            elif event == "Submit":
+                try:
+                    userAR = float(values["arBox"])
+                    if 0 > userAR or userAR > 10: raise Exception()
+                    break
+                except:
+                    window["result"].update("Invalid AR!")
+        arPopup.close()
+        if not userAR == 0:
+            for line in range(len(mapData)):
+                if mapData[line].startswith("ApproachRate:"):
+                    mapData[line] = f"ApproachRate:{userAR}\n"
+        for line in range(startIndex, len(mapData)):
+            try:
+                commaIndex = trimmer(mapData[line], ",", 2)
+                storage = f'{mapData[line][:commaIndex]}{int(int(mapData[line][commaIndex:mapData[line].find(",", commaIndex+1)])/rate)}'
+                if ("|" in mapData[line]) or (trimmer(mapData[line], ",", 6) < 5):
+                    storage += f'{mapData[line][trimmer(mapData[line], ",", 3)-1:]}'
+                else:
+                    storage += f'{mapData[line][trimmer(mapData[line], ",", 3)-1:trimmer(mapData[line], ",", 5)]}{int(int(mapData[line][trimmer(mapData[line], ",", 5):trimmer(mapData[line], ",", 6)-1])/rate)}{mapData[line][trimmer(mapData[line], ",", 6)-1:]}'
+
+                mapData[line] = storage
+            except:
+                pass
+    elif gamemode == 1:
+        for line in range(startIndex, len(mapData)):
+            try:
+                commaIndex = trimmer(mapData[line], ",", 2)
+                mapData[line] = f'{mapData[line][:commaIndex]}{int(int(mapData[line][commaIndex:mapData[line].find(",", commaIndex+1)])/rate)}{mapData[line][trimmer(mapData[line], ",", 3)-1:]}'
+            except:
+                pass
+    elif gamemode == 2:
+        for line in range(startIndex, len(mapData)):
+            try:
+                commaIndex = trimmer(mapData[line], ",", 2)
+                storage = f'{mapData[line][:commaIndex]}{int(int(mapData[line][commaIndex:mapData[line].find(",", commaIndex+1)])/rate)}'
+                if ("|" in mapData[line]) or (trimmer(mapData[line], ",", 6) < 5):
+                    storage += mapData[line][trimmer(mapData[line], ",", 3) - 1 :]
+                else:
+                    storage += f'{mapData[line][trimmer(mapData[line], ",", 3)-1:trimmer(mapData[line], ",", 5)]}{int(int(mapData[line][trimmer(mapData[line], ",", 5):trimmer(mapData[line], ",", 6)-1])/rate)}{mapData[line][trimmer(mapData[line], ",", 6)-1:]}'
+
+                mapData[line] = storage
+            except:
+                pass
+    elif gamemode == 3:
+        for line in range(startIndex, len(mapData)):
+            try:
+                commaIndex = trimmer(mapData[line], ",", 2)
+                mapData[line] = f'{mapData[line][:commaIndex]}{int(int(mapData[line][commaIndex:mapData[line].find(",", commaIndex+1)])/rate)}{mapData[line][trimmer(mapData[line], ",", 3)-1:trimmer(mapData[line], ",", 5)]}{int(int(mapData[line][trimmer(mapData[line], ",", 5):mapData[line].find(":")])/rate)}{mapData[line][mapData[line].find(":"):]}'
+            except:
+                pass
+    else:
+        print("uh waltuh")
+
+    # Creates new audio file with rate change
+    mp3Audio = AudioSegment.from_file(f"{path}/{audio}.mp3", format="mp3")
+    fileExport = speedChange(mp3Audio, rate).export(f"{path}/{audio} ({rate}x).mp3", format="mp3")
+
+    # Exports .osu! data to a file
+    fileSave = open(f"{mapFile[:mapFile.find('.osu')]} ({rate}x).osu", "w+")
+    for line in mapData:
+        fileSave.write(line)
 
 sg.theme('dark grey 9')
-
 if os.path.exists("osuRateConfig.cfg") and os.path.exists(open("osuRateConfig.cfg", "r").readline()):
     path = open("osuRateConfig.cfg", "r").readline()
 else:
@@ -27,7 +127,7 @@ else:
 
 os.chdir(path)
 folderList = sg.Combo(sorted(os.listdir()), size = (40,8), key = "folders")
-diffList = sg.Listbox(list([]), size=(50,6), enable_events=False, key='_LIST_')
+diffList = sg.Listbox(list([]), size = (100,6), enable_events=True, key='_LIST_')
 layout = [
     [sg.Text("Folder Name: "), folderList, sg.Button("Search/Select", key = "searchButton")],
     [sg.Text("Difficulty: "), diffList], 
@@ -42,16 +142,24 @@ while True:
     if event == sg.WIN_CLOSED:
         break
     elif event == "searchButton":
-        updatedFolders = sorted(filter(lambda x: values["folders"] in x, os.listdir()))
+        os.chdir(path)
+        updatedFolders = sorted(filter(lambda x: values["folders"].lower() in x.lower(), os.listdir()))
+        if(values["folders"] == updatedFolders[updatedFolders.index(values["folders"])]): updatedFolders = [updatedFolders[updatedFolders.index(values["folders"])]]
         window['folders'].update(value=updatedFolders[0], values=updatedFolders)
         if(os.path.exists(path + updatedFolders[0])):
             os.chdir(path + updatedFolders[0])
-            window['_LIST_'].update(filter(lambda x: x.endswith(".osu"),os.listdir()))
+            songs = list(filter(lambda x: x.endswith(".osu"),os.listdir()))
+            window['_LIST_'].update(songs)
     elif event == "submitButton":
+        
         try:
-            print(f"{path}{updatedFolders[0]}/{values['_LIST_']}")
-            generateFiles(f"{path}{updatedFolders[0]}/{diffList[0]}",float(values["inputRate"]))
-        except :
-            window["resultText"].update("Error, something went wrong")
+            chosenDiff = songs[window.Element('_LIST_').Widget.curselection()[0]]
+            window["resultText"].update("Working...")
+            generateFiles(path + updatedFolders[0],chosenDiff,float(values["inputRate"]))
+            window["resultText"].update("New difficulty and audio successfully created! Search for an empty string to change the rate of another map!")
+        except ValueError:
+            window["resultText"].update("Error, Invalid Rate")
+        except:
+            window["resultText"].update("Error, Something Went Wrong")
 
 window.close()
